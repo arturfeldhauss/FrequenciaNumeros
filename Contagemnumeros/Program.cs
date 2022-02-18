8f03bb3f2071a18bee1acb72d669d264d3c3a760
ALUNO <ALUNO@DESKTOP-MV721S6>
2022-02-18 16:25:53 -0300

    Add project files.

---
+++ Contagemnumeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Contagemnumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite os numeros!!");
+            string[] strnumeros = Console.ReadLine().Split(' ');
+            int[] numeros = new int[4];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = Convert.ToInt32(strnumeros[i]);
+            }
+            for(int i = 0;i < numeros.Length;i++)
+            {
+                Console.WriteLine("{0}, {0], {0} e {0}", strnumeros[i], strnumeros[0], strnumeros[1], strnumeros[2]);
+            }
+            
+            
+
+        
+        
+        
+        
+        
+          Console.ReadLine();
+        }
+    }
+}
